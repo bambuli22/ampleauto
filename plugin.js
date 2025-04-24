510572b187556a544e982743005eeec7092929e7
bambuli22 <167991436+bambuli22@users.noreply.github.com>
2025-04-24 22:26:23 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,12 +1,10 @@
-(() => {
-  return {
-    insertText: {
-      "SystemInbox": async function(app) {
-        const tagName = "system/inbox";
-        const notes = await app.filterNotes({ tag: tagName });
-        const links = notes.map(n => `- [${n.name}](https://www.amplenote.com/notes/${n.uuid})`);
-        await app.context.replaceSelection("__ Inbox:\n" + links.join("\n"));
-      }
+export default {
+  insertText: {
+    "SystemInbox": async function(app) {
+      const tagName = "system/inbox";
+      const notes = await app.filterNotes({ tag: tagName });
+      const links = notes.map(n => `- [${n.name}](https://www.amplenote.com/notes/${n.uuid})`);
+      await app.context.replaceSelection("__ Inbox:\n" + links.join("\n"));
     }
-  };
-})()
+  }
+};
