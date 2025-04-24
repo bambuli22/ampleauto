8ad30b0e23a2ec19eeca9599db89728cbd1af6c4
bambuli22 <167991436+bambuli22@users.noreply.github.com>
2025-04-24 21:53:35 +0200

    Update plugin.js
    fixed Amplenote plugin format

--- plugin.js
+++ plugin.js
@@ -1,8 +1,8 @@
-{
+export default {
   insertText: {
     "SystemInbox": async function(app) {
       const tagName = "system/inbox";
-      const notes = await app.filterNotes({tag: tagName});
+      const notes = await app.filterNotes({ tag: tagName });
       const links = notes.map(n => `[${n.name}](https://www.amplenote.com/notes/${n.uuid})`);
       await app.context.replaceSelection("- Inbox:\n" + links.map(l => `- ${l}`).join("\n"));
     }
